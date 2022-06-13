6271b9865fdd8d4808f08807fad13edb3ddd8a11
Pauline447 <paulinelechler@gmail.com>
2022-06-13 14:50:56 +0200

    Interaction UI

--- Jello-Light-Test6-Project/Assets/Scripts/CallingFriendsUI.cs
+++ Jello-Light-Test6-Project/Assets/Scripts/CallingFriendsUI.cs
@@ -6,22 +6,26 @@ using UnityEngine.UI;
 public class CallingFriendsUI : MonoBehaviour
 {
     public Sprite interationLeft;
-    public Sprite interationRight;
-    public Sprite interationUp;
+    //public Sprite interationRight;
+    //public Sprite interationUp;
     public Sprite interationDown;
-    public Sprite interationDownandUp;
-    public Sprite interationDownandLeft;
-    public Sprite interationDownandRight;
+    //public Sprite interationDownandUp;
+    //public Sprite interationDownandLeft;
+    //public Sprite interationDownandRight;
     public Sprite interationUpandLeft;
-    public Sprite interationUpandRight;
-    public Sprite interationLeftandRight;
-    public Sprite interationAll;
+    //public Sprite interationUpandRight;
+    //public Sprite interationLeftandRight;
+    //public Sprite interationAll;
     public Sprite zero;
     private Image UIimage;
 
     public int interactionCase;
     public GameObject UIinteraction;
+    
     public InteractionPossible m_interactionPossible;
+    public InteractionPossible m_interactionPossible1;
+    public InteractionPossible m_interactionPossible2;
+    public InteractionPossible m_interactionPossible3;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +39,21 @@ public class CallingFriendsUI : MonoBehaviour
         if(m_interactionPossible.interactionPossibleBool)
         {
             interactionCase = m_interactionPossible.friendCase;
+            ChangeUI();
+        }
+        else if (m_interactionPossible1.interactionPossibleBool)
+        {
+            interactionCase = m_interactionPossible1.friendCase;
+            ChangeUI();
+        }
+        else if (m_interactionPossible2.interactionPossibleBool)
+        {
+            interactionCase = m_interactionPossible2.friendCase;
+            ChangeUI();
+        }
+        else if (m_interactionPossible3.interactionPossibleBool)
+        {
+            interactionCase = m_interactionPossible3.friendCase;
             ChangeUI();
         }
         else
@@ -54,36 +73,36 @@ public class CallingFriendsUI : MonoBehaviour
             case 1:
                 UIimage.sprite = interationLeft;
                 break;
-            case 2:
-                UIimage.sprite = interationRight;
-                break;
-            case 3:
-                UIimage.sprite = interationUp;
-                break;
+            //case 2:
+            //    UIimage.sprite = interationRight;
+            //    break;
+            //case 3:
+            //    UIimage.sprite = interationUp;
+            //    break;
             case 4:
                 UIimage.sprite = interationDown;
                 break;
-            case 5:
-                UIimage.sprite = interationDownandUp;
-                break;
-            case 6:
-                UIimage.sprite = interationDownandLeft;
-                break;
-            case 7:
-                UIimage.sprite = interationDownandRight;
-                break;
+            //case 5:
+            //    UIimage.sprite = interationDownandUp;
+            //    break;
+            //case 6:
+            //    UIimage.sprite = interationDownandLeft;
+            //    break;
+            //case 7:
+            //    UIimage.sprite = interationDownandRight;
+            //    break;
             case 8:
                 UIimage.sprite = interationUpandLeft;
                 break;
-            case 9:
-                UIimage.sprite = interationUpandRight;
-                break;
-            case 10:
-                UIimage.sprite = interationLeftandRight;
-                break;
-            case 11:
-                UIimage.sprite = interationAll;
-                break;
+            //case 9:
+            //    UIimage.sprite = interationUpandRight;
+            //    break;
+            //case 10:
+            //    UIimage.sprite = interationLeftandRight;
+            //    break;
+            //case 11:
+            //    UIimage.sprite = interationAll;
+            //    break;
 
 
         }
