4394be0ee3b4c51b41291906df053d4809975f07
Pauline447 <paulinelechler@gmail.com>
2022-06-20 15:23:04 +0200

    Worm WIP

--- Jello-Light-Test6-Project/Assets/GetWormToKick.cs
+++ Jello-Light-Test6-Project/Assets/GetWormToKick.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class GetWormToKick : MonoBehaviour
 {
     public Animator anim2;
+    public GameObject worm;
+    public Transform target;
+    public float positionX;
+
+    public float followSpeed = 3f;
     public Animator anim;
+
+    public bool stopFollowing = false;
     // Start is called before the first frame update
+
+    private void Update()
+    {
+        if(stopFollowing)
+        {
+            worm.transform.position = Vector3.Lerp(worm.transform.position, target.position, followSpeed * Time.deltaTime);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
+            //worm.transform.position = Vector3.Lerp(worm.transform.position, target.position, followSpeed * Time.deltaTime);
+            stopFollowing = true;
             anim.SetBool("Kicked", true);
             anim2.SetBool("IsLit", true);
+           // Dest
         }
     }
 }
