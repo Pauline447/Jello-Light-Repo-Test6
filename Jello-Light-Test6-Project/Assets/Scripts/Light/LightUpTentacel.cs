c762eb3cbef9365f86d10b8e866499d722f97b1a
Pauline447 <paulinelechler@gmail.com>
2022-07-11 19:51:04 +0200

    Particles get activated if you place a friend

--- Jello-Light-Test6-Project/Assets/Scripts/Light/LightUpTentacel.cs
+++ Jello-Light-Test6-Project/Assets/Scripts/Light/LightUpTentacel.cs
@@ -5,16 +5,10 @@ using UnityEngine.Rendering.Universal;
 
 public class LightUpTentacel : MonoBehaviour
 {
-    public Light2D[] lights;
-    public float[] startTime;
-
-    public float[] maxLuminosity; // max intensity
-
-    public float luminositySteps = 0.001f; // factor when increasing / decreasing
-
+    public GameObject[] _particles;
     public bool startFade = false;
-    public float timer = 0.0f;
-    public int NumberOfLights = 0;
+    public int TentacelCase;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,81 +19,18 @@ public class LightUpTentacel : MonoBehaviour
     {
         if (startFade)
         {
-            timer += Time.deltaTime;
-            //if(NumberOfLights == 16)
-            //{
-            if (timer > startTime[0])
+            switch(TentacelCase)
             {
-                StartCoroutine(ChangeIntensity(lights[0], maxLuminosity[0]));
+                case 0:
+                    _particles[0].SetActive(true);
+                    break;
+                case 1:
+                   _particles[1].SetActive(true);
+                    break;
+                case 2:
+                    _particles[2].SetActive(true);
+                    break;
             }
-            if (timer > startTime[1])
-            {
-                StartCoroutine(ChangeIntensity(lights[1], maxLuminosity[1]));
-            }
-            if (timer > startTime[2])
-            {
-                StartCoroutine(ChangeIntensity(lights[2], maxLuminosity[2]));
-            }
-            if (timer > startTime[3])
-            {
-                StartCoroutine(ChangeIntensity(lights[3], maxLuminosity[3]));
-            }
-            //if (timer > startTime[4])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[4], maxLuminosity[4]));
-            //}
-            //if (timer > startTime[5])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[5], maxLuminosity[5]));
-            //}
-            //if (timer > startTime[6])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[6], maxLuminosity[6]));
-            //}
-            //if (timer > startTime[7])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[7], maxLuminosity[7]));
-            //}
-            //if (timer > startTime[8])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[8], maxLuminosity[8]));
-            //}
-            //if (timer > startTime[9])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[9], maxLuminosity[9]));
-            //}
-            //if (timer > startTime[10])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[10], maxLuminosity[10]));
-            //}
-            //if (timer > startTime[11])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[11], maxLuminosity[11]));
-            //}
-            //if (timer > startTime[12])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[12], maxLuminosity[12]));
-            //}
-            //if (timer > startTime[13])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[13], maxLuminosity[13]));
-            //}
-            //if (timer > startTime[14])
-            //{
-            //    StartCoroutine(ChangeIntensity(lights[14], maxLuminosity[14]));
-            //}
-            //  }
-            //for-Schleife nicht möglich, da das eine schon angefangen muss während das andere noch in der Coroutine is...
         }
-    }
-    private IEnumerator ChangeIntensity(Light2D l, float maxLumi)
-    {
-        while (l.intensity <= maxLumi)
-        {
-            l.intensity += luminositySteps; // increase the firefly intensity / fade in
-            yield return new WaitForEndOfFrame();
-        }
-
-        yield return new WaitForSeconds(0f); // wait 3 seconds
     }
 }
