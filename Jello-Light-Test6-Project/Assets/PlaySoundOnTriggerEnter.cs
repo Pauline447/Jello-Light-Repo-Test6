caabd053e956670314c4b0a1aabffd9098400607
Pauline447 <paulinelechler@gmail.com>
2022-07-16 23:54:51 +0200

    Enviroment Sounds fixed

--- Jello-Light-Test6-Project/Assets/PlaySoundOnTriggerEnter.cs
+++ Jello-Light-Test6-Project/Assets/PlaySoundOnTriggerEnter.cs
@@ -8,22 +8,14 @@ public class PlaySoundOnTriggerEnter : MonoBehaviour
     public MMFeedbacks _FeedbacksPlay;
     public MMFeedbacks _FeedbacksResume;
     public MMFeedbacks _FeedbacksStop;
-    private bool doneOnce = false;
+
     // Start is called before the first frame update
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            if(!doneOnce)
-            {
                 _FeedbacksPlay.PlayFeedbacks();
-                doneOnce = true;
-            }
-            else
-            {
-                _FeedbacksResume.PlayFeedbacks();
-            }
         }
     }
     private void OnTriggerExit2D(Collider2D other)
