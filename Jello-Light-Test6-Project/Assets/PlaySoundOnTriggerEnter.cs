db96a17603f8e46192dfe2d6f16df7deb52d298e
Pauline447 <paulinelechler@gmail.com>
2022-07-17 10:33:12 +0200

    Area Sounds work, Anglerfisch area doesnt, scream doesnt

--- Jello-Light-Test6-Project/Assets/PlaySoundOnTriggerEnter.cs
+++ Jello-Light-Test6-Project/Assets/PlaySoundOnTriggerEnter.cs
@@ -6,22 +6,32 @@ using MoreMountains.Feedbacks;
 public class PlaySoundOnTriggerEnter : MonoBehaviour
 {
     public MMFeedbacks _FeedbacksPlay;
-    public MMFeedbacks _FeedbacksResume;
     public MMFeedbacks _FeedbacksStop;
-
+    public MMFeedbacks _FeebacksResume;
+    private bool doneonce = false;
     // Start is called before the first frame update
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
+            if(!doneonce)
+            {
                 _FeedbacksPlay.PlayFeedbacks();
+                doneonce = true;
+            }
+            else
+            {
+                _FeebacksResume.PlayFeedbacks();
+            }
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+
+            _FeedbacksPlay.StopFeedbacks();
             _FeedbacksStop.PlayFeedbacks();
         }
     }
